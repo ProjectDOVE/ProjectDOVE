235919532e6b947829ef914dee05bc8a558dc726
BlackScorp <vitalij@blackscorp.de>
2015-11-04 19:38:44 +0100

    initialized slim

---
+++ bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of the ProjectDOVE.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+error_reporting(-1);
+ini_set('display_errors',1);
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+return $app;
---
+++ web/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file is part of the ProjectDOVE.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$app = require_once __DIR__.'/../bootstrap.php';
+
+$app->run();

33a9173581c0c22ef866e01f696853ff97bd5e5f
Pandoria <lisa.saalfrank@gmx.de>
2015-11-06 18:55:38 +0100

    Added login response object
    Self descriptive

---
+++ src/Response/LoginResponse.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Dove\Response;
+
+/**
+ * This file is part of the ProjectDOVE.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+class LoginResponse extends BaseResponse
+{
+
+    /**
+     * @access  public
+     * @return  string  $username   The login name
+     */
+    public function username() {
+        return $this->request->post('username');
+    }
+    
+    /**
+     * @access  public
+     * @return  string  $password   The raw password
+     */
+    public function password() {
+        return $this->request->post('password');
+    }
+}
